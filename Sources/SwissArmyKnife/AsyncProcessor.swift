5405f56b7c9e1894f64a90b48b181ea12e9c9a68
Łukasz Łazarecki <lazarecki@gmail.com>
2026-01-23 10:41:59 +0100

    Async processor added

---
+++ Sources/SwissArmyKnife/AsyncProcessor.swift
@@ -0,0 +1,71 @@
+//
+//  AsyncBufferProcessor.swift
+//  LiveTrackerDemo
+//
+//  Created by Łukasz Łazarecki on 22/01/2026.
+//
+
+import Foundation
+
+public final class AsyncProcessor<T> {
+    private let dataQueue = DispatchQueue(label: "AsyncProcessor.dataQueue", qos: .userInitiated)
+    private let workQueue = DispatchQueue(label: "AsyncProcessor.workQueue", qos: .userInitiated)
+
+    private let semaphore = DispatchSemaphore(value: 0)
+
+    public private(set) var dataBufferSize:Int
+    private var data:[T] = []
+    private var isRunning = false
+
+    public init(dataBufferSize:Int) {
+        self.dataBufferSize = dataBufferSize
+    }
+
+    public func startProcessing(_ worker: @escaping (_ data: T) -> Void) {
+        dataQueue.sync {
+            guard !isRunning else { fatalError("processing already started") }
+            isRunning = true
+        }
+
+        workQueue.async { [weak self] in
+            while true {
+                guard let self else { return }
+                
+                let runningBefore = self.dataQueue.sync { self.isRunning }
+                guard runningBefore else { break }
+
+                self.semaphore.wait()
+
+                let runningAfter = self.dataQueue.sync { self.isRunning }
+                guard runningAfter else { break }
+
+                let dataToProcess: T? = self.dataQueue.sync { !self.data.isEmpty ? self.data.removeFirst() : nil }
+
+                guard let dataToProcess else { continue }
+                worker(dataToProcess)
+            }
+        }
+    }
+
+    public func stopProcessing() {
+        dataQueue.sync {
+            isRunning = false
+            data.removeAll()
+        }
+
+        semaphore.signal()
+    }
+
+    public func addData(_ data: T) {
+        dataQueue.async(flags: .barrier) {
+            self.data.append(data)
+            
+            if self.data.count > self.dataBufferSize {
+                self.data.removeFirst()
+            }
+            
+            self.semaphore.signal()
+        }
+    }
+}
+
