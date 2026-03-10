26da31eb44f0b77097165953dc7ecb829c0fc0aa
Łukasz Łazarecki <lazarecki@gmail.com>
2026-03-10 13:04:59 +0100

    GPUImage3 added as a dependency

--- Package.swift
+++ Package.swift
@@ -17,6 +17,9 @@ let package = Package(
             name: "SwissArmyKnife",
             targets: ["SwissArmyKnife"]),
     ],
+    dependencies: [
+        .package(path: "../GPUImage3")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
